04ef2c12241bdeea9cacead24da3d2f5896a9b27
fatema71 <fatema.tee.08@gmail.com>
2015-05-13 16:47:05 +0600

    Hello

---
+++ FileOperations/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileOperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\103407\02052015\FileOperations\text.txt";
+            FileStream aFileStream=new FileStream (path, FileMode.Append);
+            StreamWriter aStreamWriter = new StreamWriter(aFileStream);
+            aStreamWriter.WriteLine(nameTextBox.Text);
+            aStreamWriter.Close();
+            aFileStream.Close();
+            nameTextBox.Clear();
+        }
+    }
+}
